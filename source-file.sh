c420d07cce697a48b9d17f4869021064177025c1
TK <thomas.kidd@okstate.edu>
2024-12-06 12:30:44 -0600

    adding submodules

---
+++ source-file.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Set the environment variables
+export LABEL_STUDIO_URL="http://0.0.0.0:8080/projects/?page=1"
+export LABEL_STUDIO_API_KEY="4b2be218f8c03a1719933db947f045def3a3aa9f"
+export MAX_FRAMES_TO_TRACK="25" # Adjust based on your need
+export PROMPT_TYPE="box"         # or "point"
+export SEGMENT_ANYTHING_2_REPO_PATH="/media/zappi/main-ssd/Projects/Cornhole+/project-2/segment-anything-2"
+export LOG_LEVEL="DEBUG"
+export DEBUG="true"
+
+# Confirm the variables are set
+echo "Environment variables set:"
+echo "LABEL_STUDIO_URL=$LABEL_STUDIO_URL"
+echo "LABEL_STUDIO_API_KEY=$LABEL_STUDIO_API_KEY"
+echo "MAX_FRAMES_TO_TRACK=$MAX_FRAMES_TO_TRACK"
+echo "PROMPT_TYPE=$PROMPT_TYPE"
+echo "SEGMENT_ANYTHING_2_REPO_PATH=$SEGMENT_ANYTHING_2_REPO_PATH"
+
